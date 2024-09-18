ab2881c9f4377a9e9e92e9e8a2148ce130408d5d
Lionportal1 <134019334+Lionportal1@users.noreply.github.com>
2024-09-18 10:39:44 -0500

    v.1.0.0 Commit

---
+++ DataTypes.java
@@ -0,0 +1,29 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			int x = 1975;
+			int y = 2024;
+			int z = y - x;
+			
+			int birthyear = 1970;
+			int currentyear = 2024;
+			int age = currentyear - birthyear;
+			
+			float f = 0.5f;
+			double f2 = 1.5555555;
+			
+			char c = 'M';
+			char d = 10;
+			
+			char e = '7';
+			
+			boolean TF = !true;
+			
+			System.out.println();
+		}
+	}
+
+}
